5c4f05a8a05a180fda793e2f3c28c89cb40453ed
JasonChiang <jiangzhe1224@gmail.com>
2015-08-02 16:59:13 +0800

    UI Setup Step2

---
+++ ChannelController.swift
@@ -0,0 +1,46 @@
+//
+//  ChannelController.swift
+//  DoubanFM
+//
+//  Created by JasonChiang on 8/02/15.
+//  Copyright (c) 2015年 JasonChiang. All rights reserved.
+//
+
+import UIKit
+
+class ChannelController: UIViewController {
+
+    @IBOutlet weak var tblChannelList: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tblChannelList.dequeueReusableCellWithIdentifier("channelitem") as! UITableViewCell
+        cell.textLabel?.text = "ChannelName: \(indexPath.row)"
+        cell.imageView?.image = UIImage(named: "detail")
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DoubanFM/ViewController.swift
+++ DoubanFM/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var imageArtwork: ImagePostEffect!
     @IBOutlet weak var imageBg: UIImageView!
+    @IBOutlet weak var tblSongList: UITableView!
 
     func onBlurEffect(imageView: UIImageView?) {
         if (imageView != nil) {
@@ -29,6 +30,9 @@ class ViewController: UIViewController {
 
         imageArtwork.onRotation()
         onBlurEffect(imageBg)
+
+        tblSongList.dataSource = self
+        tblSongList.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,6 +40,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 9
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tblSongList.dequeueReusableCellWithIdentifier("songitem") as! UITableViewCell
+        cell.textLabel?.text = "Title: \(indexPath.row)"
+        cell.detailTextLabel?.text = "Detail: \(indexPath.row)"
+        cell.imageView?.image = UIImage(named: "detail")
+        return cell
+    }
 }
 
